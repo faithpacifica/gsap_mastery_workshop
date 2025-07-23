a4a8c627f006735ae88dafcec31da36ee54290ce
Dildora Muslimova <faithpacifica@yahoo.com>
2025-07-24 00:33:09 +0900

    add some comments

--- pages/16-scrubbing_animation copy/script.js
+++ pages/16-scrubbing_animation copy/script.js
@@ -13,7 +13,7 @@ gsap.to(".box", {
   stagger: 0.1, //har bir elementga 0.1 sekund kechikish bilan animatsiya qo'llanadi
   ease: "power1.inOut",
   scrollTrigger: {
-    trigger: ".box",
+    trigger: ".container",
     start: "top center",
     end: "bottom 10%",
     scrub: 3, //3 sekund ichida animatsiya tugaydi
@@ -29,9 +29,12 @@ gsap.to(".box1", {
   scrollTrigger: {
     trigger: ".box1",
     start: "top center",
-    end: "bottom 20%", //20% ga kelganda tugaydi
+    end: "bottom 20%", //20% ga kelganda tugaydi  bu albatta scrollTrigger ichida bo'lishi kerak bulmasa animatsiya ishlamaydi!!!!!!!
     markers: true,
     scrub: true, // bu yerda animatsiya scroll bilan bog'liq, ya'ni foydalanuvchi pastga siljiganda animatsiya ham siljiydi
   },
 });
 
+// trigger: ".container"	We’re telling ScrollTrigger to watch the .container element to decide when to start the animation.
+// start: "top center"	This means the animation will begin when the top of the container hits the center of the viewport.
+// end: "bottom 20%"	It will finish when the bottom of the container hits 20% from the top of the viewport — giving us a nice, long scroll area to play with.
